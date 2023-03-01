421f0b7e538cb9d0e288d85d59a7d6cc452eb786
seele508 <hanspeterwibowo@gmail.com>
2023-03-01 15:16:33 +0700

    coba dummy
    dummy

---
+++ Models/Producer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading.Tasks;
+using Confluent.Kafka;
+
+namespace SchTracer.Models
+{
+    class cProducer
+    {
+        //public static async Task Main(string[] args)
+        public static async Task Wolololo()
+        {
+            var config = new ProducerConfig { BootstrapServers = "https://kafka5.dev.bri.co.id:9021" };
+
+            // If serializers are not specified, default serializers from
+            // `Confluent.Kafka.Serializers` will be automatically used where
+            // available. Note: by default strings are encoded as UTF8.
+            using (var p = new ProducerBuilder<Null, string>(config).Build())
+            {
+                try
+                {
+                    var dr = await p.ProduceAsync("test-topic", new Message<Null, string> { Value = "test" });
+                    Console.WriteLine($"Delivered '{dr.Value}' to '{dr.TopicPartitionOffset}'");
+                }
+                catch (ProduceException<Null, string> e)
+                {
+                    Console.WriteLine($"Delivery failed: {e.Error.Reason}");
+                }
+            }
+        }
+    }
+}
